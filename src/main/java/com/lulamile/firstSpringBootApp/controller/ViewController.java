dc604e1acf6f638099284af6bcd702f7a6537947
lulamile99 <lulabenni45@gmail.com>
2024-01-13 14:40:13 +0200

    Adding footer and header components

--- src/main/java/com/lulamile/firstSpringBootApp/controller/ViewController.java
+++ src/main/java/com/lulamile/firstSpringBootApp/controller/ViewController.java
@@ -1,11 +1,13 @@
-package com.lulamile.firstSpringBootApp.view;
+package com.lulamile.firstSpringBootApp.controller;
 
 import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController {
+public class ViewController {
     @GetMapping("/home")
     public String home(Model model){
         return "home";
