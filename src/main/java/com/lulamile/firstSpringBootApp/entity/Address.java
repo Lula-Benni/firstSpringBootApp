58b7703d580c32b629645560c211017e8b524d68
lulamile99 <lulabenni45@gmail.com>
2024-02-14 02:03:01 +0200

    Removed @Data annotation and used getter and setter adding to string and hashCode methods excluding profiles in both toString and hashCode

--- src/main/java/com/lulamile/firstSpringBootApp/entity/Address.java
+++ src/main/java/com/lulamile/firstSpringBootApp/entity/Address.java
@@ -1,15 +1,14 @@
 package com.lulamile.firstSpringBootApp.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -31,4 +30,29 @@ public class Address {
     private String additionalDetails;
     @OneToMany(mappedBy = "address")
     private List<Profile> profiles;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Address address)) return false;
+        return addressId == address.addressId && Objects.equals(street, address.street) && Objects.equals(city, address.city) && Objects.equals(postalCode, address.postalCode) && Objects.equals(state, address.state) && Objects.equals(country, address.country) && Objects.equals(additionalDetails, address.additionalDetails);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(addressId, street, city, postalCode, state, country, additionalDetails);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "addressId=" + addressId +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", additionalDetails='" + additionalDetails + '\'' +
+                '}';
+    }
 }
